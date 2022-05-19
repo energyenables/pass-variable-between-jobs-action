c77c95edf0824c9680d3315cc65d1c7b64f31884
Jamie Williams <mail@jamiewilliams.co.uk>
2022-05-19 18:27:14 +0100

    Create action

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 import * as core from "@actions/core";
 import * as artifacts from "@actions/artifact";
 
-const run = async (): Promise<void> => {
-  const name = core.getInput("name", { required: true });
-  const path = core.getInput("path", { required: true });
+const ROOT_DIRECTORY = '/tmp/variables';
 
+const setVariable = async (name: string, value: string): Promise<void> => {
   const client = artifacts.create();
+  const filePath = path.join(ROOT_DIRECTORY, name);
+  await fs.writeFile(filePath, value);
+  await client.uploadArtifact(name, [filePath], ROOT_DIRECTORY);
+  core.info(`Set variable ${name} successfully.`);
+};
+
+const getVariable = async (name: string): Promise<void> => {
+  const client = artifacts.create();
+  const filePath = path.join(ROOT_DIRECTORY, name);
+  await client.downloadArtifact(filePath);
+  const file = await fs.readFile(filePath)
+  core.setOutput(name, file.toString());
+  core.info(`Got variable ${name} successfully.`);
+};
+
+const run = async (): Promise<void> => {
+  const mode = core.getInput('mode', { required: true });
+  const name = core.getInput('name', { required: true })
+  const value = core.getInput('value', { required: true })
+
+  if (mode === 'set') await setVariable(name, value);
+  if (mode === 'get') await getVariable(name);
 };
 
 run();
